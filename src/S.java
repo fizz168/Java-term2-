9d5b6bd6feddd27fa43a582e8486510b72a8e0e2
fizz168 <nheanomra@gmail.com>
2026-01-24 11:52:08 +0700

    Fix array initialization and sum calculation in S class

--- src/S.java
+++ src/S.java
@@ -1,9 +1,12 @@
 public class S {
     public static void main(String[] args) {
-        int[] value = new int[5];
+        int[] value = {1, 2, 3, 4};
+        int sum = 0;
         for(int i = 0; i < value.length; i++){
-            System.out.println(value);
+            sum += value[i]; //
+            // System.out.println(value[i]);
         }
+        System.out.println("sum number of the array is " + sum);
         
         String FirstName = "Omra";
         String LastName = "Nhean";
