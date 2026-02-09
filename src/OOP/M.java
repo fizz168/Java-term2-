6e4dd6b90622c3cc3e3fa1e07141a61f84ace3f4
fizz168 <nheanomra@gmail.com>
2026-02-09 22:36:19 +0700

    update main

--- src/OOP/M.java
+++ src/OOP/M.java
@@ -12,16 +12,33 @@ public class M { // this is main for the oop i ran out of idea for name
 
 
 
-CoffeeShop shop = new CoffeeShop("omra", 10);
+// CoffeeShop shop = new CoffeeShop("omra", 10);
 //build menu 
-shop.addMenuItem("nigga", 1.5);
-shop.addMenuItem("nig", 1.5);
-shop.addMenuItem("nigg", 1.5);
-shop.showMenu();
+// shop.addMenuItem("nigga", 1.5);
+// shop.addMenuItem("nig", 1.5);
+// shop.addMenuItem("nigg", 1.5);
+// shop.showMenu();
 
 //create order 
 // Order order = new Order(5);
 // order.getTotal();
+CoffeeShop shop = new CoffeeShop("CADT", 20, 20);
+//set menu 
+shop.addMenuItem("mocha", 2.5);
+shop.addMenuItem("h", 8.2);
+shop.addMenuItem("ha", 2.5);
+shop.addMenuItem("g", 8.2);
 
+Buyer buyer = new Buyer("omra", 50.00, true);
+Seller seller = new Seller("nigga", 140.00);
+
+System.out.println();
+shop.showMenu();
+
+double originalPrice = 2.5;
+double copiedPrice = originalPrice;
+copiedPrice = 3.5;
+System.out.println("o" + originalPrice);
+System.out.println("c" + copiedPrice);
     }
 }
