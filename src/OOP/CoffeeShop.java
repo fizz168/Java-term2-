b81b1d6043f0865fcec4792bc96db6ef481f9ccc
fizz168 <nheanomra@gmail.com>
2026-02-09 22:26:52 +0700

    update : coffeeShop

--- src/OOP/CoffeeShop.java
+++ src/OOP/CoffeeShop.java
@@ -23,14 +23,29 @@
 package OOP;
 
 public class CoffeeShop {
-    String name;
+    String shopName;
+    
     Menu[] menu;
     int menuCount;
 
-    public CoffeeShop(String name, int maxMenuItems) {
-        this.name = name;
-        menu = new Menu[maxMenuItems];
-        menuCount = 0;
+    Order[] orders;
+    int orderCount;
+
+    int nextOrderId;
+
+
+
+    public CoffeeShop(String shopName, int menuCapacity, int orderCapacity) {
+        this.shopName = shopName;
+
+        this.menu = new Menu[menuCapacity];
+        this.menuCount = 0;
+
+        this.orders = new Order[orderCapacity];
+        this.orderCount = 0;
+
+        this.nextOrderId = 1; // want next order to start from 1 cus 0 already store last order
+      
     }
 
     public void addMenuItem(String name, double price) {
@@ -43,7 +58,7 @@ public class CoffeeShop {
     }
 
     public void showMenu() {
-        System.out.println("Menu of " + name + ":");
+        System.out.println("Menu of " + shopName + ":");
         for (int i = 0; i < menuCount; i++) {
             System.out.println(menu[i].name + " : $" + menu[i].price);
         }
